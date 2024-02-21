4e4457bf03184f45cdea91781b6bce231ce33080
Luke Buchanan <luke1020will@gmail.com>
2024-02-20 20:14:59 -0500

    set some stuff

--- lua/luke/set.lua
+++ lua/luke/set.lua
@@ -13,3 +13,15 @@ vim.opt.backup = false
 vim.opt.swapfile = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.g.mapleader = " "
