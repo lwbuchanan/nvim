0d4ef541f6bbed1b11bc402716c64f80ef50d09d
Luke Buchanan <luke1020will@gmail.com>
2024-02-21 15:50:43 -0500

    added some plugins

--- lua/luke/set.lua
+++ lua/luke/set.lua
@@ -20,7 +20,8 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.isfname:append("@-@")
+vim.opt.signcolumn = "no"
+--vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
